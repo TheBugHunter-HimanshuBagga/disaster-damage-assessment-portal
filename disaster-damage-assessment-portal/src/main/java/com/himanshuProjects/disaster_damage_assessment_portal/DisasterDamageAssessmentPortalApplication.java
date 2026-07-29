07f8605b32f2bfa34a6e3c1b136582008f7aa2a3
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-30 00:28:14 +0530

    Project set-Up

---
+++ disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/DisasterDamageAssessmentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.himanshuProjects.disaster_damage_assessment_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterDamageAssessmentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterDamageAssessmentPortalApplication.class, args);
+	}
+
+}
---
+++ disaster-damage-assessment-portal/src/test/java/com/himanshuProjects/disaster_damage_assessment_portal/DisasterDamageAssessmentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshuProjects.disaster_damage_assessment_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterDamageAssessmentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
