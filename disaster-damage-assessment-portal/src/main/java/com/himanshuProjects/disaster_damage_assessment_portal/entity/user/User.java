befbfc8516ed8665f35683a4f89bf0cb392dcccc
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-30 21:43:25 +0530

    feat(user): implement User entity with validation and district mapping

--- disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/user/User.java
+++ disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/user/User.java
@@ -1,4 +1,70 @@
 package com.himanshuProjects.disaster_damage_assessment_portal.entity.user;
 
-public class User {
+import com.himanshuProjects.disaster_damage_assessment_portal.common.BaseEntity;
+import com.himanshuProjects.disaster_damage_assessment_portal.enums.AccountStatus;
+import com.himanshuProjects.disaster_damage_assessment_portal.enums.Gender;
+import com.himanshuProjects.disaster_damage_assessment_portal.enums.RoleType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(
+        name = "users"// unique constraints in the database tell it that don['t allow the duplicate values
+//        uniqueConstraints = {
+//                @UniqueConstraint(columnNames = "email"),
+//                @UniqueConstraint(columnNames = "phone_number")
+//        }
+)
+public class User extends BaseEntity {
+
+    @NotBlank(message = "Full Name is required")
+    @Size(max = 100, message = "Full name cannot exceed 100 characters")
+    @Column(name = "full_name", nullable = false, length = 100)
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please enter a valid email address")
+    @Size(max = 150, message = "Email cannot exceed 150 characters")
+    @Column(nullable = false, unique = true , length = 150)
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^[6-9]\\d{9}$",
+            message = "Please enter a valid 10-digit Indian mobile number"
+    )
+    @Column(name = "phone_number", nullable = false , unique = true , length = 10)
+    private String phoneNumber;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 255, message = "Password must be between 8 to 255 characters")
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private RoleType role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION; // Updated to ACTIVE after otp_verification
+
+    // external fields
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "district_id", nullable = false)
+    private District district;
+
 }
