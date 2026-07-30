384629df9720fb2bbc39b8d043d6ea3cc52b774a
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-30 19:19:20 +0530

    feat(district): implement district entity with state relationship

--- disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/user/District.java
+++ disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/user/District.java
@@ -1,4 +1,31 @@
 package com.himanshuProjects.disaster_damage_assessment_portal.entity.user;
 
-public class District {
+import com.himanshuProjects.disaster_damage_assessment_portal.common.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(
+        name = "districts",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"name","state_id"}) // district name can't be used inside the same state
+        }
+)
+public class District extends BaseEntity {
+
+    @NotBlank(message = "District name is required")
+    @Size(max = 100, message = "District name cannot exceed 100 characters")
+    @Column(name = "name", nullable = false, length = 100) // database column named as "name"
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY) // many districts comes under 1 state
+    @JoinColumn(name = "state_id", nullable = false)
+    private State state;
 }
