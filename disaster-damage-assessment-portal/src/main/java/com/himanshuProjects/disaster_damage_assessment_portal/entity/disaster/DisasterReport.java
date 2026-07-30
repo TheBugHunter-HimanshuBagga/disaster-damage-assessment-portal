c51ad87ef21f596725aa560539c339594c35772c
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-31 01:31:27 +0530

    feat(disaster): implement DisasterReport entity

--- disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/disaster/DisasterReport.java
+++ disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/disaster/DisasterReport.java
@@ -1,4 +1,63 @@
 package com.himanshuProjects.disaster_damage_assessment_portal.entity.disaster;
 
-public class DisasterReport {
+import com.himanshuProjects.disaster_damage_assessment_portal.common.BaseEntity;
+import com.himanshuProjects.disaster_damage_assessment_portal.entity.user.User;
+import com.himanshuProjects.disaster_damage_assessment_portal.enums.DisasterType;
+import com.himanshuProjects.disaster_damage_assessment_portal.enums.ReportStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "disaster_reports")
+public class DisasterReport extends BaseEntity { // main entity created By citizens when reporting a disaster
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 150, message = "Title cannot exceed 150 characters")
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 1000, message = "Description cannot exceed 1000 characters")
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Disaster type is required")
+    @Column(name = "disaster_type", nullable = false, length = 30)
+    private DisasterType disasterType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private ReportStatus status = ReportStatus.SUBMITTED;
+
+    @NotBlank(message = "Incident Address is required")
+    @Size(max = 500, message = "Incident address cannot exceed 500 characters")
+    @Column(name = "incident_address", nullable = false, length = 500)
+    private String incidentAddress;
+
+    @NotNull(message = "Latitude is required")
+    @Column(nullable = false)
+    private Double latitude;
+
+    @NotNull(message = "Longitude is required")
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(name = "reported_at", nullable = false)
+    private LocalDateTime reportedAt = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "citizen_id", nullable = false)
+    private User citizen;
+
 }
