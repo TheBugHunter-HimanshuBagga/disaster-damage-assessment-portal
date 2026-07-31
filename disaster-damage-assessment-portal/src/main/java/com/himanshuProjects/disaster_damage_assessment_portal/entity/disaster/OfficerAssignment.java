67cafae4e086ee53ec2b2622b407798733b04903
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-31 23:17:38 +0530

    feat(officer-assignment): implement officer assignment entity

--- disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/disaster/OfficerAssignment.java
+++ disaster-damage-assessment-portal/src/main/java/com/himanshuProjects/disaster_damage_assessment_portal/entity/disaster/OfficerAssignment.java
@@ -1,4 +1,35 @@
 package com.himanshuProjects.disaster_damage_assessment_portal.entity.disaster;
 
-public class OfficerAssignment {
+import com.himanshuProjects.disaster_damage_assessment_portal.common.BaseEntity;
+import com.himanshuProjects.disaster_damage_assessment_portal.entity.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "officer_assignments")
+public class OfficerAssignment extends BaseEntity {
+    // which offices is assigned to inspect this disaster report
+
+    @OneToOne(fetch = FetchType.LAZY) // one office has assigned one disaster report
+    @JoinColumn(name = "disaster_report_id", nullable = false)
+    private DisasterReport disasterReport;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "field_officer_id", nullable = false)
+    private User fieldOfficer;
+
+    @Column(name = "assigned_at" ,nullable = false)
+    private LocalDateTime assignedAt = LocalDateTime.now();
+
+    @NotNull(message = "Estimated Inspection date is required")
+    @Column(name = "inspection_date", nullable = false)
+    private LocalDateTime inspectionDate;
+
 }
